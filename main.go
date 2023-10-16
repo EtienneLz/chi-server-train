bf677f3a0df277b1cb90c37a99681915c88c3fd5
Etienne Louchez <etienne.louchez@digimobee.com>
2023-10-16 16:51:49 +0200

    Added purchase list to db Fixed duplicate entry in purchase table Handled database disconnection

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"go_train/data"
 	"go_train/server"
 	"os"
+	"time"
 )
 
 func main() {
@@ -13,8 +15,22 @@ func main() {
 		println("Use \"./main.exe server_name server\" or \"./main.exe server_name db-update\" instead")
 		os.Exit(0)
 	}
-	data.G_DB = server.DbConnect(os.Args[1])
+	var ok bool
+	data.G_DB, ok = server.DbConnect(os.Args[1])
+	if ok == false {
+		println("Please verify that your database is running")
+		return
+	}
 
+	ticker := time.NewTicker(30 * time.Second)
+	go func() {
+		for t := range ticker.C {
+			println("Pinging Database...")
+			fmt.Println("Tick at", t)
+			checkDatabase()
+		}
+
+	}()
 	if os.Args[2] == "server" {
 		server.Init()
 	} else if os.Args[2] == "db-update" {
@@ -24,4 +40,21 @@ func main() {
 		println("Incorrect argument")
 		println("Use \"./main.exe server_name server\" or \"./main.exe server_name db-update\"")
 	}
+	select {}
+}
+
+func checkDatabase() {
+	err := data.G_DB.Ping()
+	var ok bool
+	if err != nil {
+		println("Connection to Database lost, trying to reconnect...")
+		for ok != true {
+			data.G_DB, ok = server.DbConnect(os.Args[1])
+			if ok != true {
+				println("Connection to Database lost, trying to reconnect...")
+			}
+			time.Sleep(5 * time.Second)
+		}
+	}
+	println("Database online")
 }
--- server/server.go
+++ server/server.go
@@ -142,47 +142,76 @@ func addToDb(customerRef string, purchaseList []string) {
 	_, err := data.G_DB.Exec("INSERT INTO user_update (remote_customer_reference, last_update_date) VALUES (?, ?) "+
 		"ON DUPLICATE KEY UPDATE last_update_date = ?", customerRef, dateFormatted, dateFormatted)
 	if err != nil {
+		println("Error 503")
 		println(err.Error())
 	}
 
-	_, err = data.G_DB.Exec("INSERT INTO user_purchase (remote_customer_reference) VALUES (?)", customerRef)
+	_, err = data.G_DB.Exec("INSERT INTO user_purchase (remote_customer_reference) VALUES (?) "+
+		"ON DUPLICATE KEY UPDATE purchase_lists = ?", customerRef, customerRef)
 	if err != nil {
+		println("Error 504")
 		println(err.Error())
 	}
 
 	var columnName string
 	for _, str := range purchaseList {
-
+		println(str)
 		data.G_DB.QueryRow("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ? AND COLUMN_NAME = ?",
 			"user_purchase", str).Scan(&columnName)
 		if columnName == "" {
-			_, err = data.G_DB.Exec("ALTER TABLE user_purchase ADD COLUMN " + str + " int")
+			_, err = data.G_DB.Exec("ALTER TABLE user_purchase ADD COLUMN " + str + " int NOT NULL DEFAULT 0")
 			if err != nil {
+				println("Error 500")
+				log.Fatal(err)
+			}
+			_, err = data.G_DB.Exec("UPDATE user_purchase SET "+str+" = 1 WHERE remote_customer_reference = ?", customerRef)
+			if err != nil {
+				println("Error 501")
 				log.Fatal(err)
 			}
 		} else {
-			// Column exists
+			_, err = data.G_DB.Exec("UPDATE user_purchase SET " + str + " = " + str + " + 1 WHERE remote_customer_reference = '" + customerRef + "'")
+			if err != nil {
+				println("Error 502")
+				log.Fatal(err)
+			}
 		}
+		columnName = ""
 	}
 }
 
-func DbConnect(name string) *sql.DB {
+func DbConnect(name string) (*sql.DB, bool) {
+	var connected bool
 	db, err := sql.Open("mysql", "root:pass@tcp(0.0.0.0:3308)/"+name)
+	err = db.Ping()
 	if err != nil {
-		panic(err.Error())
+		connected = false
+	} else {
+		connected = true
 	}
 	//defer db.Close()
+	//if connected == true {
+	//	_, err = db.Exec("CREATE TABLE IF NOT EXISTS " + name + ".user_update(remote_customer_reference varchar(50), last_update_date varchar(10), PRIMARY KEY (remote_customer_reference))")
+	//	if err != nil {
+	//		panic(err.Error())
+	//	}
+	//
+	//	_, err = db.Exec("CREATE TABLE IF NOT EXISTS " + name + ".user_purchase(remote_customer_reference varchar(50), purchase_lists varchar(50), PRIMARY KEY (remote_customer_reference))")
+	//	if err != nil {
+	//		panic(err.Error())
+	//	}
+	//
+	//	_, err = db.Exec("ALTER TABLE user_purchase ADD UNIQUE (`remote_customer_reference`)")
+	//	if err != nil {
+	//		println(err.Error())
+	//	}
+	//}
 
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS " + name + ".user_update(remote_customer_reference varchar(50), last_update_date varchar(10), PRIMARY KEY (remote_customer_reference))")
-	if err != nil {
-		panic(err.Error())
+	if connected == true {
+		println("Connected to Database")
+	} else {
+		println("Connection to Database failed")
 	}
 
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS " + name + ".user_purchase(remote_customer_reference varchar(50), PRIMARY KEY (remote_customer_reference))")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	println("Connection to Database successful")
-	return db
+	return db, connected
 }
